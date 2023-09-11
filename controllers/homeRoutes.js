1474be8f019cad79b3f713a61266028986f012f5
Andrei <andrei.ribeirow@gmail.com>
2023-09-11 15:22:37 +1000

    adding view files main.handlebars layot and nav.handlebars partials, and also the homepage.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,11 +14,10 @@ router.get('/', async (req, res) => {
 		const posts = postData.map((post) => post.get({
 			plain: true
 		}));
-        res.json(postData);
-		// res.render('homepage', {
-		// 	posts,
-		// 	logged_in: req.session.logged_in
-		// });
+		res.render('homepage', {
+			posts,
+			logged_in: req.session.logged_in
+		});
 	} catch (err) {
 		res.status(500).json(err);
 	}
