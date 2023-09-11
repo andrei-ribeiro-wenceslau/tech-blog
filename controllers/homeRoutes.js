79054401079eed4cf82c74f289e926785be8cdc1
Andrei <andrei.ribeirow@gmail.com>
2023-09-11 22:39:59 +1000

    just formating code from all files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,94 +3,99 @@ const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-	try {
-		const postData = await Post.findAll({
-			include: [{
-				model: User,
-				attributes: ['username'],
-			},],
-		});
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
 
-		const posts = postData.map((post) => post.get({
-			plain: true
-		}));
-		res.render('homepage', {
-			posts,
-			logged_in: req.session.logged_in
-		});
-	} catch (err) {
-		res.status(500).json(err);
-	}
+    const posts = postData.map((post) =>
+      post.get({
+        plain: true,
+      })
+    );
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/post/:id', async (req, res) => {
-	try {
-		const postData = await Post.findByPk(req.params.id, {
-			include: [
-				{
-					model: User,
-					attributes: ['username'],
-				}, {
-					model: Comment,
-					include: [
-						User
-					]
-				}
-			],
-		});
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
 
-		const post = postData.get({
-			plain: true
-		});
+    const post = postData.get({
+      plain: true,
+    });
 
-		res.render('post', {
-			...post,
-			logged_in: req.session.logged_in
-		});
-	} catch (err) {
-		res.status(500).json(err);
-	}
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/dashboard', withAuth, async (req, res) => {
-	try {
-		const userData = await User.findByPk(req.session.user_id, {
-			attributes: {
-				exclude: ['password']
-			},
-			include: [{
-				model: Post
-			}],
-		});
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: {
+        exclude: ['password'],
+      },
+      include: [
+        {
+          model: Post,
+        },
+      ],
+    });
 
-		const user = userData.get({
-			plain: true
-		});
+    const user = userData.get({
+      plain: true,
+    });
 
-		res.render('dashboard', {
-			...user,
-			logged_in: true
-		});
-	} catch (err) {
-		res.status(500).json(err);
-	}
+    res.render('dashboard', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', (req, res) => {
-	if (req.session.logged_in) {
-		res.redirect('/dashboard');
-		return;
-	}
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
 
-	res.render('login');
+  res.render('login');
 });
 
 router.get('/signUp', (req, res) => {
-	if (req.session.logged_in) {
-		res.redirect('/dashboard');
-		return;
-	}
-	res.render('signUp');
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('signUp');
 });
 
 module.exports = router;
--- public/js/comments.js
+++ public/js/comments.js
@@ -1,30 +1,27 @@
-// Define an asynchronous function that handles form submissions for creating comments
 const commentFormHandler = async function (event) {
-	event.preventDefault(); // Prevent the default form submission behavior
+  event.preventDefault(); // Prevent the default form submission behavior
 
-	// Get the blog ID and comment description from the form fields
-	const blog_id = document.querySelector('.new-comment-form').dataset.blogid;
-	const comment_description = document.querySelector('#comment_description').value.trim();
+  const post_id = document.querySelector('.new-comment-form').dataset.postid;
+  const comment_content = document
+    .querySelector('#comment_content')
+    .value.trim();
 
-	// If a comment description was entered, make a POST request to the server to create the comment
-	if (comment_description) {
-		await fetch('/api/comments', {
-			method: 'POST',
-			body: JSON.stringify({
-				blog_id,
-				comment_description,
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		});
+  if (comment_content) {
+    await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        post_id,
+        comment_content,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-		// Reload the page to display the new comment
-		document.location.reload();
-	}
+    document.location.reload();
+  }
 };
 
-// Attach the comment form submission handler to the form's submit event
 document
-	.querySelector('.new-comment-form')
-	.addEventListener('submit', commentFormHandler);
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', commentFormHandler);
