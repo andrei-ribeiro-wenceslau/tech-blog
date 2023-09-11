7da7a8e4de10cd465f76db33dd1a73cec926b472
Andrei <andrei.ribeirow@gmail.com>
2023-09-11 17:11:58 +1000

    added post.handlebars for the post page and also login, signup and dashboard handlebars. Added user routes and post routes, and also added the js file for the sign in

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
-// const router = require('express').Router();
-// const postRoutes = require('./postRoutes.js');
+const router = require('express').Router();
+const postRoutes = require('./postRoutes.js');
 // const commentRoutes = require('./commentRoutes.js');
-// const userRoutes = require('./userRoutes.js');
+const userRoutes = require('./userRoutes.js');
 
-// router.use('/posts', postRoutes);
+router.use('/posts', postRoutes);
 // router.use('/comments', commentRoutes);
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 
-// module.exports = router;
+module.exports = router;
